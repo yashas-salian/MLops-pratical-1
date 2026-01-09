b1e0231195e18f9cd07384a3176de2b2d07e68e2
Your Name <youremail@example.com>
2026-01-09 09:42:17 +0530

    new version

--- pracical-2.py
+++ pracical-2.py
@@ -14,10 +14,10 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42
+    X_scaled, y, test_size=0.4, random_state=42
 )
 
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors=7)
 knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 
